c4b5bf08f4a75847d6f6b90aee08ced5927ec3fe
tbgbarros <tbg.barros@hotmail.com>
2022-11-03 22:34:29 -0300

    alteracao principal e index principal

--- index2.php
+++ index2.php
@@ -222,67 +222,6 @@
 
 
 
-<div class="container">
-	<div class="d-flex justify-content-center h-100">
-		<div class="card">
-			<div class="card-header">
-				<h3>Sign In</h3>
-				<div class="d-flex justify-content-end social_icon">
-					<span><i class="fab fa-facebook-square"></i></span>
-					<span><i class="fab fa-google-plus-square"></i></span>
-					<span><i class="fab fa-twitter-square"></i></span>
-				</div>
-			</div>
-			<div class="card-body">
 
-			
-            <?php if(isset($_SESSION['loginErro'])){
-                echo $_SESSION['loginErro'];
-                unset($_SESSION['loginErro']);
-            }?>
-        </p>
-        <p>
-            <?php 
-            if(isset($_SESSION['logindeslogado'])){
-                echo $_SESSION['logindeslogado'];
-                unset($_SESSION['logindeslogado']);
-            }
-            ?>
-	   
-
-				<form method="POST" action="valida.php">
-				<h2>Área Restrita. Faça login para continuar no sitema. Obrigado!</h2>
-					<div class="input-group form-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-user"></i></span>
-						</div>
-						<input class="form-control" type="email" name="email" placeholder="Email">
-						
-					</div>
-					<div class="input-group form-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-key"></i></span>
-						</div>
-						<input class="form-control" type="password" name="senha" placeholder="Senha">
-					</div>
-					<div class="row align-items-center remember">
-						<input type="checkbox">Remember Me
-					</div>
-					<div class="form-group">
-						<input type="submit" value="Login" class="btn float-right login_btn">
-					</div>
-				</form>
-			</div>
-			<div class="card-footer">
-				<div class="d-flex justify-content-center links">
-					Don't have an account?<a href="#">Sign Up</a>
-				</div>
-				<div class="d-flex justify-content-center">
-					<a href="#">Forgot your password?</a>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
 </body>
 </html>
